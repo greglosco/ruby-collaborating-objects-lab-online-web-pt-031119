32dc5d4a61b03729f213fcc8f97c0e92e1ac1e2b
Gregory Losco <gtlosco@gmail.com>
2019-04-23 04:12:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,7 @@ class Song
     artist, song = file.split(" - ")
     new_song = self.new(song)
     new_artist = Artist.find_or_create_by_name(new_artist)
+    
     new_song.artist = new_artist
     new_song
   end
