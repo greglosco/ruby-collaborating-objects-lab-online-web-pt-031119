9ba860c7e69180e4552ac76c0c249419d58008f9
Gregory Losco <gtlosco@gmail.com>
2019-04-23 04:00:18 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song 
+  
+  attr_accessor :name, :artist
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.new_by_filename(file_name)
+    artist, song = file_name.split(" - ")
+    new_song = self.new(song)
+    new_artist = Artist.find_or_create_by_name(artist)
+    new_song.artist = new_artist
+    new_song
+  end
+  
+end
